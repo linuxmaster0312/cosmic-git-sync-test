bf06d6e0d5757768a9d05896416bb9021f36d4be
Josh Park <joshua.park0312@gmail.com>
2025-07-28 13:18:27 -0700

    init commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center size-full">
+      <h1 className="text-4xl font-bold">Start Building Your Project!</h1>
+    </div>
+  );
+}
