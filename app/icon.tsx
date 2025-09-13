e565611269c23971a72ac5052e453bc12cfb8484
Josh Park <joshua.park0312@gmail.com>
2025-09-12 22:39:30 -0700

    added visual editor related code

--- app/icon.tsx
+++ app/icon.tsx
@@ -3,32 +3,32 @@ import { ImageResponse } from 'next/og';
 
 export const size = {
   width: 64,
-  height: 64,
+  height: 64
 };
 export const contentType = 'image/png';
 
 export default function Icon() {
   return new ImageResponse(
-    (
-      <div
-        style={{
-          width: '100%',
-          height: '100%',
-          display: 'flex',
-        }}
-      >
-        <img
-          src="https://storage.googleapis.com/cosmic-project-image-assets/images/c14a2256-a303-4bac-a5fb-bb9e62cd4f1a/favicon.png" // Replace with your public image URL
-          style={{
-            width: '100%',
-            height: '100%',
-            objectFit: 'contain',
-          }}
-        />
-      </div>
-    ),
+
+    <div data-editor-id="app/icon.tsx:13:7"
+    style={{
+      width: '100%',
+      height: '100%',
+      display: 'flex'
+    }}>
+
+        <img data-editor-id="app/icon.tsx:20:9"
+      src="https://storage.googleapis.com/cosmic-project-image-assets/images/c14a2256-a303-4bac-a5fb-bb9e62cd4f1a/favicon.png" // Replace with your public image URL
+      style={{
+        width: '100%',
+        height: '100%',
+        objectFit: 'contain'
+      }} />
+
+      </div>,
+
     {
-      ...size, 
+      ...size
     }
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center size-full">
-      <h1 className="text-4xl font-bold">Start Building Your Cosmic Project</h1>
-    </div>
-  );
+    <div data-editor-id="app/page.tsx:5:5" className="flex flex-col items-center justify-center size-full">
+      <h1 data-editor-id="app/page.tsx:6:7" className="text-4xl font-bold">Start Building Your Cosmic Project</h1>
+    </div>);
+
 }
